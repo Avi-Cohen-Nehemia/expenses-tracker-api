923f209d568bdc5db2cdaf53b83b34458b05182c
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-09-07 10:04:14 +0100

    added api index route to get all expenses

--- app/Http/Controllers/API/Expenses.php
+++ app/Http/Controllers/API/Expenses.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Expense;
 
 class Expenses extends Controller
 {
@@ -14,7 +15,7 @@ class Expenses extends Controller
      */
     public function index()
     {
-        //
+        return Expense::all();
     }
 
     /**
