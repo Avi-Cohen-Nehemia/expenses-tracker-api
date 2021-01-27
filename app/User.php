089633aa1aab3501b0666f7fa85986de5844d10f
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-27 14:16:36 +0000

    run migrations and add HasApiTokens trait to User model

--- app/User.php
+++ app/User.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Database\Factories\UserFactory;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     protected $fillable = [
         'name', 'email', 'password',
