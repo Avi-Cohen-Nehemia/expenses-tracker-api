73359eb0df632f36b09fa38201a7074039367860
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-28 16:43:30 +0000

    token is sent along with user name and id

---
+++ app/Http/Controllers/API/Login.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class Login extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'name' => ['required'],
+            'password' => ['required']
+        ]);
+
+        $user = User::where('name', $request->name)->first();
+
+        if (!$user || $request->password !== $user->password) {
+            throw ValidationException::withMessages([
+                'name' => ["incorrect credentials"]
+            ]);
+        }
+
+        return response()->json([
+            "id" => $user->id,
+            "name" => $user->name,
+            "access_token" => $user->createToken("Auth Token")->accessToken,
+        ]);
+    }
+}
