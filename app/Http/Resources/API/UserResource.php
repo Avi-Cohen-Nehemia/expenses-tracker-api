5c1cd77d5b79db406aee8a3f9b9a03bcc9ce38b0
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-01 13:39:54 +0000

    make api send calcualted total of expense and income

--- app/Http/Resources/API/UserResource.php
+++ app/Http/Resources/API/UserResource.php
@@ -23,11 +23,32 @@ class UserResource extends JsonResource
         $collection = collect($this->transactions);
         $sorted = $collection->sortByDesc('created_at');
 
+        $income = $collection->filter(function ($transaction, $key) {
+            return $transaction->type === "income";
+        });
+
+        $expense = $collection->filter(function ($transaction, $key) {
+            return $transaction->type === "expense";
+        });
+
+        $totalIncome = $income->reduce(function ($acc, $transaction) {
+            return $acc + $transaction->amount;
+        }, 0);
+
+        $totalExpense = $expense->reduce(function ($acc, $transaction) {
+            return $acc + $transaction->amount;
+        }, 0);
+
+        $formattedTotalIncome = FormatToCurrency::toCurrency($totalIncome);
+        $formattedTotalExpense = FormatToCurrency::toCurrency($totalExpense);
+
         return [
             "id" => $this->id,
             "name" => $this->name,
             "email" => $this->email,
             "balance" => $formattedBalance,
+            "total_income" => $formattedTotalIncome,
+            "total_expense" => $formattedTotalExpense,
             "transactions" => TransactionResource::collection($sorted),
         ];
     }
