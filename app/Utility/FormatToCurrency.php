cddcf7445eacc00735d055a4b3b1d96e0f246a92
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-17 11:48:07 +0000

    change how NumberFormatter is referenced

--- app/Utility/FormatToCurrency.php
+++ app/Utility/FormatToCurrency.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace App\Utility;
-use NumberFormatter;
 
 class FormatToCurrency
 {
     public static function toCurrency(float $amount) : string
     {
-        $format = new NumberFormatter( 'en_GB', NumberFormatter::CURRENCY );
+        $format = new \NumberFormatter( 'en_GB', \NumberFormatter::CURRENCY );
         $formattedAmount = $format->formatCurrency($amount, "GBP");
 
         return $formattedAmount;
