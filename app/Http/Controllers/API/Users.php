99a41aa69b68633bda177fed6d21f489cd647805
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-05 11:17:11 +0000

    divide error messages returned by title and text

--- app/Http/Controllers/API/Users.php
+++ app/Http/Controllers/API/Users.php
@@ -43,14 +43,29 @@ class Users extends Controller
         } catch (QueryException $error) {
 
             if (isset($request->name)) {
-                return response()->json(['error' => "Username already taken"], Response::HTTP_CONFLICT);
+                return response()->json([
+                    'error' => [
+                        "title" => "Username already taken",
+                        "text" => "Please try a different username"
+                    ]
+                ], Response::HTTP_CONFLICT);
             }
 
             if (isset($request->email)) {
-                return response()->json(['error' => "Email already taken"], Response::HTTP_CONFLICT);
+                return response()->json([
+                    'error' => [
+                        "title" => "Email already taken",
+                        "text" => "Please try a different email"
+                    ]
+                ], Response::HTTP_CONFLICT);
             }
 
-            return response()->json(['error' => "Something went wrong"], Response::HTTP_BAD_REQUEST);
+            return response()->json([
+                'error' => [
+                    "title" => "Oops... Something went wrong",
+                    "text" => "Please try again"
+                ] 
+            ], Response::HTTP_BAD_REQUEST);
         }
     }
 
