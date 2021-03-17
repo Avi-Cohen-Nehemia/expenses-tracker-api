75335346f18c20eb09d166eb88ed08375be1e078
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-17 11:36:41 +0000

    replace hard coded formatting of totals with a custom utility class

--- app/Http/Resources/API/TransactionsByDateRangeResource.php
+++ app/Http/Resources/API/TransactionsByDateRangeResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\API;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\API\TransactionResource;
+use App\Utility\FormatToCurrency;
 use App\Utility\UserFunds;
 
 class TransactionsByDateRangeResource extends JsonResource
@@ -26,9 +27,9 @@ class TransactionsByDateRangeResource extends JsonResource
 
         return [
             "total_income" => floatval($totalIncome),
-            "total_income_with_currency" => "£{$totalIncome}",
+            "total_income_with_currency" => FormatToCurrency::toCurrency($totalIncome),
             "total_expense" => floatval($totalExpense),
-            "total_expense_with_currency" => "£{$totalExpense}",
+            "total_expense_with_currency" => FormatToCurrency::toCurrency($totalExpense),
             "total_expense_by_category" => $totalExpenseByCategory,
             "transactions" => $transactions,
         ];
